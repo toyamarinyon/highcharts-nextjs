8a7edf7023e08be583e3e11a57312ceb0c7ec0e9
toyamarinyon <toyamarinyon@gmail.com>
2025-03-24 23:18:12 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -14,76 +14,71 @@ import { apiDataToSankeyData, data } from "./data";
 // like Options come from the Highcharts module itself.
 
 export default function Page() {
-  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
-  const options = useMemo<Highcharts.Options>(() => {
-    const sankeyData = apiDataToSankeyData(data);
-    console.log("???");
-    console.log(sankeyData);
+	const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+	const options = useMemo<Highcharts.Options>(() => {
+		const sankeyData = apiDataToSankeyData(data);
 
-    return {
-      chart: {
-        styledMode: true,
-        className: "highcharts-light",
-      },
-      plotOptions: {
-        sankey: {
-          colorByPoint: true,
-          curveFactor: 0,
-          linkOpacity: 0.2,
-          nodePadding: 30,
-          dataLabels: {
-            align: "left",
-            verticalAlign: "top",
-            nodeFormat: "{point.name}<br /><strong>{point.sum}</strong>",
-            x: 20,
-          },
-        },
-      },
-      title: {
-        text: undefined,
-      },
-      series: sankeyData,
-    };
-  }, []);
+		return {
+			chart: {
+				styledMode: true,
+				className: "highcharts-light",
+			},
+			plotOptions: {
+				sankey: {
+					colorByPoint: true,
+					curveFactor: 0,
+					linkOpacity: 0.2,
+					nodePadding: 30,
+					dataLabels: {
+						align: "left",
+						verticalAlign: "top",
+						nodeFormat: "{point.name}<br /><strong>{point.sum}</strong>",
+						x: 20,
+					},
+				},
+			},
+			title: {
+				text: undefined,
+			},
+			series: sankeyData,
+		};
+	}, []);
 
-  return (
-    <div>
-      <nav className="flex items-center justify-between w-full px-[10px] bg-[#FBF8FF] h-[32px] border-b border-[#999999]">
-        <div className="flex items-center w-full text-[12px] text-[#4976E6] font-bold">
-          <div className="flex items-center flex-1">
-            <div className="flex-1">流入キーワード</div>
-            <div className="flex-1">情報収集ページ</div>
-            <div className="flex-1">評価検討ページ</div>
-            <div className="w-[20px] relative h-full flex">
-              <div className="absolute right-0 -translate-y-[50%] w-[100px] text-right">
-                意思決定ページ
-              </div>
-            </div>
-          </div>
-          <div className="w-[190px] text-center">
-            <button type="button">CVを追加する</button>
-          </div>
-        </div>
-      </nav>
-      <div className="flex h-[400px]">
-        <div className="flex-1 h-full">
-          <HighchartsReact
-            highcharts={Highcharts}
-            options={options}
-            ref={chartComponentRef}
-          />
-        </div>
-        <div className="w-[190px] flex items-center justify-center p-[24px]">
-          <div className="border-dotted border-[#999999] border-[3px] h-full w-full bg-[#EBEBEB] flex items-center justify-center rounded-[4px]">
-            <button
-              type="button"
-              className="border-[2px] border-[#4976E6] rounded-[4px] h-[40px] px-[16px] bg-[#FFFFFF] text-[#4976E6] cursor-pointer"
-            >
-              CVを追加
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<nav className="flex items-center justify-between w-full px-[10px] bg-[#FBF8FF] h-[32px] border-b border-[#999999]">
+				<div className="flex items-center w-full text-[12px] text-[#4976E6] font-bold">
+					<div className="flex items-center flex-1">
+						{data?.map((data) => (
+							<div className="flex-1" key={data.type}>
+								{data.type}
+							</div>
+						))}
+					</div>
+					<div className="w-[190px] text-center">
+						<button type="button">CVを追加する</button>
+					</div>
+				</div>
+			</nav>
+			<div className="flex h-[400px]">
+				<div className="flex-1 h-full">
+					<HighchartsReact
+						highcharts={Highcharts}
+						options={options}
+						ref={chartComponentRef}
+					/>
+				</div>
+				<div className="w-[190px] flex items-center justify-center p-[24px]">
+					<div className="border-dotted border-[#999999] border-[3px] h-full w-full bg-[#EBEBEB] flex items-center justify-center rounded-[4px]">
+						<button
+							type="button"
+							className="border-[2px] border-[#4976E6] rounded-[4px] h-[40px] px-[16px] bg-[#FFFFFF] text-[#4976E6] cursor-pointer"
+						>
+							CVを追加
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
